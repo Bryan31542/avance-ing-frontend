3c4711f35946f67f7a82bd58587e1f8987b93412
Bryan31542 <00063820@uca.edu.sv>
2024-03-24 14:55:07 -0600

    :sparkles: Feat: add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-4xl font-mono font-black">Hello world</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/login" element={<div>login</div>} />
+        <Route path="/register" element={<div>register</div>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
